e3cce613197168818d34c4d7e80a2a5ecda701cf
Mayra Guevara <maystef1@hotmail.com>
2019-06-09 21:09:20 -0600

    definicion de las d emas propiedades como BX,CX,DX aun sigue dando error el PropertyChange

--- CaluloRectangulo/CaluloRectangulo/ClaseRectangulo.cs
+++ CaluloRectangulo/CaluloRectangulo/ClaseRectangulo.cs
@@ -42,7 +42,59 @@ namespace CaluloRectangulo
             }
         }
 
-        
+        public string BX
+        {
+            get { return bx; }
+            set
+            {
+                int numero;
+                bool resultado = int.TryParse(value, out numero);
+                if (resultado) bx = value;
+                OnPropertyChanged("BX");
+                OnPropertyChanged("Altura");
+                OnPropertyChanged("Base");
+
+            }
+
+
+        }
+
+        public string CX
+        {
+            get { return cx; }
+            set
+            {
+                int numero;
+                bool resultado = int.TryParse(value, out numero);
+                if (resultado) cx = value;
+                OnPropertyChanged("CX");
+                OnPropertyChanged("Altura");
+                OnPropertyChanged("Base");
+
+            }
+
+
+        }
+
+        public string DX
+        {
+            get { return dx; }
+            set
+            {
+                int numero;
+                bool resultado = int.TryParse(value, out numero);
+                if (resultado) dx = value;
+                OnPropertyChanged("DX");
+                OnPropertyChanged("Altura");
+                OnPropertyChanged("Base");
+
+            }
+
+
+        }
+
+
+
     }
 
 
