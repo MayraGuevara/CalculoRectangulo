08aa129677df484bb0943ed711c72f587273db76
Mayra Guevara <maystef1@hotmail.com>
2019-06-09 20:27:44 -0600

    definicion de variables a usar.

--- CaluloRectangulo/CaluloRectangulo/ClaseRectangulo.cs
+++ CaluloRectangulo/CaluloRectangulo/ClaseRectangulo.cs
@@ -3,10 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel;
 
 namespace CaluloRectangulo
 {
-    class ClaseRectangulo
+    public class ClaseRectangulo : INotifyPropertyChanged
     {
+        private string ax;
+        private string ay;
+
+        private string bx;
+        private string by;
+
+        private string cx;
+        private string cy;
+
+        private string dx;
+        private string dy;
+
+        private string altura;
+        private string baseR;
+        private string perimetro;
+        private string area;
+        private bool validar;
+        private string resultado;
+       
+
+
+    }
+
+
+
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
